27a1899964448ee15c8f9806946c9d04c1f2ec91
Sh3ldone <150495179+Sh3ldone@users.noreply.github.com>
2023-12-04 17:50:17 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -13,7 +13,6 @@ with st.container():
     st.header("Hi I am Sheldone R. Dacuya :wave:")
     st.subheader("A BSCpE Student In SURIGAO DEL NORTE STATE UNIVERSITY 🏫")
 
-
 with st.container():
     st.markdown("<h1 style='text-align: right;'>Homepage</h1>", unsafe_allow_html=True)
     st.markdown("------")
@@ -22,6 +21,11 @@ with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        
+        st.write("Left column content goes here. Modify this according to your needs.")
+
+    with right_column:
+        # Right column content goes here
+        pass  # Placeholder, you can add your content here
+
 
  
