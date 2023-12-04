3bbfcced7c9c3682ed728183b7df8c0e47325695
Sh3ldone <150495179+Sh3ldone@users.noreply.github.com>
2023-12-04 17:47:34 +0800

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Music Webpage",
+    page_icon=":musical_notes:",
+    layout="wide",
+)
+
+st.title("Introduction")
+st.sidebar.success("Select a page above.")
+
+with st.container():
+    st.header("Hi I am Sheldone R. Dacuya :wave:")
+    st.subheader("A BSCpE Student In SURIGAO DEL NORTE STATE UNIVERSITY 🏫")
+
+
+with st.container():
+    st.markdown("<h1 style='text-align: right;'>Homepage</h1>", unsafe_allow_html=True)
+    st.markdown("------")
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        
+
+ 
