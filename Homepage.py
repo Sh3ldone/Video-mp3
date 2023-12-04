d850030e7fa6a81579e7bd46ae88a7d71cbfa78e
Sh3ldone <150495179+Sh3ldone@users.noreply.github.com>
2023-12-04 18:33:25 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,20 +1,5 @@
 import streamlit as st
 
-# Set dark theme using custom CSS
-dark_theme = """
-<style>
-body {
-    background-color: #1E1E1E;
-    color: #FFFFFF;
-}
-.sidebar {
-    background-color: #333333;
-}
-</style>
-"""
-
-st.markdown(dark_theme, unsafe_allow_html=True)
-
 st.set_page_config(
     page_title="My Music Webpage",
     page_icon=":musical_notes:",
@@ -36,7 +21,8 @@ with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.write("Hi there welcome to my webpage please feel free to explore my webpage ")
+        st.write("Hi there welcome to my webpage please feel freee to explore my webpage ")
+
 
 
     
