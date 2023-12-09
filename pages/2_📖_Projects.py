ad011a0657f2a1151b51f1b655be929cf692c341
Sh3ldone <150495179+Sh3ldone@users.noreply.github.com>
2023-12-09 08:53:53 +0800

    Update 2_📖_Projects.py

--- pages/2_📖_Projects.py
+++ pages/2_📖_Projects.py
@@ -1,5 +1,10 @@
 import streamlit as st 
 
 st.title("📖Projects")
+st.header("I'm a musician; that's why I created this project so that I can easily convert our band's performance into music MP3.")
 
-st.write("[convert here >](https://6a3detmwvyutciekoywziu.streamlit.app/)")
+
+st.subheader("If you wish to convert video into mp3 just click here👇")
+st.subheader("[convert here >](https://6a3detmwvyutciekoywziu.streamlit.app/)")
+
+st.subheader("Thank you for visiting my page😉")
