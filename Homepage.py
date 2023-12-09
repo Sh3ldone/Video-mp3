369f1ac3c5a3a7a200a4831dfa7c2f403682eaa4
Sh3ldone <150495179+Sh3ldone@users.noreply.github.com>
2023-12-09 12:15:01 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -21,11 +21,18 @@ with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.write("Good day, everyone! Thank you for visiting my page. I am Sheldone Dacuya, the creator of this website. I am grateful because I have learned a lot, especially in programming. On this website, I share my passion for technology, coding, and various projects that I've worked on.")
-        st.write("Explore the different sections to find insightful articles, tutorials, and updates on the latest in the tech world. Whether you're a fellow programmer or someone curious about the world of coding, I hope you find valuable information and inspiration here.")
-        st.write("Feel free to navigate through the projects I've created, each with its unique story and purpose. From programming challenges to innovative solutions, I strive to showcase the endless possibilities that coding offers.")
-        st.write("If you have any questions, feedback, or just want to say hello, don't hesitate to reach out. Your support means a lot to me, and I look forward to connecting with you on this exciting journey of exploration and learning.")
-        st.markdown("Thank you once again for being a part of this community. Happy coding! [Click here](https://www.w3schools.com/python/)")
+        st.write("""
+        Good day, everyone! Thank you for visiting my page. I am Sheldone Dacuya, the creator of this website. 
+        I am grateful because I have learned a lot, especially in programming. On this website, 
+        I share my passion for technology, coding, and various projects that I've worked on
+        Explore the different sections to find insightful articles, tutorials, and updates on the latest in the tech world.
+        Whether you're a fellow programmer or someone curious about the world of coding, I hope you find valuable information and inspiration here.
+        Feel free to navigate through the projects I've created, each with its unique story and purpose. 
+        From programming challenges to innovative solutions, I strive to showcase the endless possibilities that coding offers
+        If you have any questions, feedback, or just want to say hello, don't hesitate to reach out. 
+        Your support means a lot to me, and I look forward to connecting with you on this exciting journey of exploration and learning
+        Thank you once again for being a part of this community. Happy coding! [Click here](https://www.w3schools.com/python/)
+        """)
 
 
 
